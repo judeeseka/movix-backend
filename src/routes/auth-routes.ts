5058fabe0e01ac000e2156cab9f04831ee97d858
Jude Eseka <esekachinedu256@gmail.com>
2025-06-30 01:34:02 +0100

    change method for refresh endpoint

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.get("/refresh", refresh)
+router.post("/refresh", refresh)
 router.post("/onboard", authenticateUser, upload.single("profileImg"), onboardUser)
 
 export default router;
