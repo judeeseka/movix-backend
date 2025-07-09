2aa9128e980dbc45ff492f38182bfe816e9ddedd
Jude Eseka <esekachinedu256@gmail.com>
2025-07-09 21:30:09 +0100

    update auth middleware to handle expired token properly

--- src/middleware/auth-user.ts
+++ src/middleware/auth-user.ts
@@ -1,38 +1,45 @@
 import { NextFunction, Request, Response } from "express";
 import logger from "../utils/logger";
-import jwt from "jsonwebtoken";
+import jwt, { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 import { env } from "../config/env-config";
 import { AuthenticatedRequest } from "../interfaces/interface";
 
 export const authenticateUser = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.split(" ")[1]
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
-        res.status(401).json({
-            message: "Unauthorized"
-        })
-        return;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
     }
 
+    const token = authHeader.split(" ")[1];
+
     try {
-        const decoded = jwt.verify(token, env.JWT_SECRET)
-        
-        if (typeof decoded === "string" || !("userId" in decoded)) {
+        const decoded = jwt.verify(token, env.JWT_SECRET);
+
+        if (typeof decoded !== "object" || !("userId" in decoded)) {
             res.status(401).json({ message: "Invalid token payload" });
-            return
-          }
+            return;
+        }
 
-        req.userId = decoded.userId
-        next()
-        
+        req.userId = decoded.userId;
+        next();
     } catch (error) {
-        logger.error("Failed to authenticate user", {
-            error: (error instanceof Error) ? error.message : error,
-            stack: error instanceof Error ? error.stack : null
-        })
+        if (
+            error instanceof TokenExpiredError ||
+            error instanceof JsonWebTokenError
+        ) {
+            logger.error("Token is expired or invalid")
+            res.status(401).json({ message: "Unauthorized" });
+            return;
+        }
 
-        res.status(500).json({
-            message: "Internal server error"
-        })
+        logger.error("Unexpected error during authentication", {
+            error: error instanceof Error ? error.message : error,
+            stack: error instanceof Error ? error.stack : null,
+        });
+
+        res.status(500).json({ message: "Internal server error" });
+        return
     }
 }
